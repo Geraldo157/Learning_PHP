2df5bf59956ddacc7d9d13824971ffe50e9afb46
Geraldo157 <gsmecanica76@gmail.com>
2023-05-11 14:27:19 -0300

    Creating Finance helper

---
+++ Finance_Calculator/calculations.php
@@ -0,0 +1,27 @@
+<?php
+class calculations{
+    private $anos;
+    private $juros;
+    private $valorInvestido;
+    public function __construct($anos, $juros, $valorInvestido){
+        $this->$anos = $anos;
+        $this->$juros = $juros;
+        $this->$valorInvestido = $valorInvestido;
+    }
+
+    public function calculate(){
+        $calc = $this->valorInvestido * (1 - $this->juros) ** $this->anos;
+    }
+
+    public function getAnos(){
+        return $this->anos;
+    }
+
+    public function getJuros(){
+        return $this->juros;
+    }
+
+    public function getValorInvestido(){
+        return $this->valorInvestido;
+    }
+}
