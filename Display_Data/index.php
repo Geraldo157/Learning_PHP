e314bf3a8acd4eac1239f2a1b24faef8ecda9e8e
Geraldo157 <gsmecanica76@gmail.com>
2023-05-10 23:40:17 -0300

    Finishing project

--- Display_Data/index.php
+++ Display_Data/index.php
@@ -9,6 +9,7 @@
     <h1>Table of Transactions</h1>
     <div class="tabela">
         <?php require_once 'code.php' ?>
+        <img src="serie_a_2019_classificacao_38a_rodada_570_1.jpg">
     </div>
 </body>
 </html>
